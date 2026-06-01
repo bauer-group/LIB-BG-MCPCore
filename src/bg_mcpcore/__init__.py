5903d93447d6799bf898804d985240bf3421b8be
semantic-release <semantic-release>
2026-06-01 22:33:57 +0000

    1.3.0
    Automatically generated by python-semantic-release

--- src/bg_mcpcore/__init__.py
+++ src/bg_mcpcore/__init__.py
@@ -27,7 +27,7 @@ from __future__ import annotations
 import importlib
 from typing import TYPE_CHECKING
 
-__version__ = "1.2.1"
+__version__ = "1.3.0"
 __author__ = "BAUER GROUP"
 __email__ = "info@bauer-group.com"
 
